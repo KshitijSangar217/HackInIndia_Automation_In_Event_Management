4ad8fc25409fe1b665b47ae6c13d92e7b069f121
Kshitij Sangar <kshitijsangar@gmail.com>
2020-07-01 17:02:59 +0530

    Made Superuser

--- automation/views.py
+++ automation/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+import smtplib
+from email.message import EmailMessage
+
 
 def home(request):
-    return render(request,'home.html')
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+    s.starttls()  # Traffic encryption
+    s.login("kshitijsangarofficial@gmail.com", "myfirstloverituu@mom.com")
+
+    msg = EmailMessage()
+    msg['Subject'] = "demo"
+    msg['From'] = "kshitijsangarofficial@gmail.com"
+    msg['To'] = "kshitijsangar@gmail.com"
+    gmail_content = "Hello guys"
+    msg.set_content(gmail_content)
+
+    s.send_message(msg)
+    s.quit()
+    print('\n\n\n\n\n\n\n\n mails sent\n\n\n')
+    return render(request, 'home.html')
