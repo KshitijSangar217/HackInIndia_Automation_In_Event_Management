69e2c18054e096c7996a2fda9fd9fa51a3cddd1a
Kshitij Sangar <kshitijsangar@gmail.com>
2020-07-01 23:16:37 +0530

    Error of \\ solved.

--- automation/views.py
+++ automation/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 import smtplib
 from email.message import EmailMessage
 from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import pandas as pd
+import xlrd
+import os
+from hackathon_automate import settings
+from hackathon_automate.settings import BASE_DIR
+
+in_development = True
 
 
 def home(request):
@@ -11,7 +19,6 @@ def home(request):
 
 def returndata(request):
     if request.method == "POST":
-        print(request.GET)
         uploaded_file = request.FILES["document"]
         fs = FileSystemStorage()
         fs.save(uploaded_file.name, uploaded_file)
@@ -20,34 +27,36 @@ def returndata(request):
         s.starttls()  # Traffic encryption
         s.login("kshitijsangarofficial@gmail.com", "myfirstloverituu@mom.com")
 
-        excelFile = "DemoExcelFile.xlsx"  # Mail
+        # Variable Declarations
         emailID = "kshitijsangarofficial@gmail.com"
-        pwd = "pwd"
+        pwd = "myfirstloverituu@mom.com"
         subject = "New Video's UP"
         htmlfile_loc = "FinalTemplate.html"
-        #with open(htmlfile_loc, 'r', encoding='utf8') as file:
-         #   html_Content = str(file.read().replace('{username}', "NAME").replace('{org_image}', "org_image"))
+        # NAME : Make sure that the name column is named "NAME" and email as "EMAIL" in the excelfile
+        thumbnaillink = "https://miro.medium.com/max/1400/1*Bhqdl1UvZsuXrXbiE-ujsw.png"
+        videotitle = 'Remove ScrollBar in SwiftUI List'
+        videodescription = "When we create custom Views in SwiftUI, Sometimes Scroll Bar doesn't look nice. So, in this video, I am going to show you how to remove the scroll bar in SwiftUI List."
+        videolink = 'https://youtu.be/oPp4htuqDOs'
 
+        file = pd.ExcelFile(uploaded_file)
+        for sheet in file.sheet_names:
+            print("\n\n<-- New Sheet -->\n")
+            df1 = file.parse(sheet)
+            for i in range(len(df1['EMAIL'])):
+                # EDITING the NAME here
 
-
-
-        msg = EmailMessage()
-        msg['Subject'] = "demo"
-        msg['From'] = "kshitijsangarofficial@gmail.com"
-        msg['To'] = "kshitijsangar@gmail.com"
-        gmail_content = "Hello guys"
-        msg.set_content(gmail_content)
-
-        # Attaching the Poster
-        f = open('excelmedia/' + uploaded_file.name, 'rb')
-        fdata = f.read()
-        # fname = 'images/' + CertificateFileName
-        fname = 'DemoImage.pdf'
-
-        msg.add_attachment(fdata, maintype='application', subtype='octet-stream', filename=fname)
-
-        s.send_message(msg)
+                #directory_path = os.path.join(BASE_DIR,"automation/templates/EmailTemplate1.html")
+                with open(os.path.join(BASE_DIR,"automation/templates/ExampleTemplate1.html"), 'r', encoding='utf8') as file:
+                    html_Content = str(file.read().replace('{videodescription}', videodescription).replace('{username}',df1['NAME'][i]).replace('{videolink}', videolink).replace('{videotitle}', videotitle).replace('{thumbnaillink}',thumbnaillink))
+                msg = EmailMessage()
+                msg['Subject'] = subject
+                msg['From'] = emailID  # ===========================
+                msg['To'] = df1['EMAIL'][i]
+                msg.add_alternative(html_Content, subtype="html")
+                s.send_message(msg)
+                print("--> ", df1['SRNO'][i], ": ", df1['EMAIL'][i], " : Sent")
         s.quit()
         data = "sent to all"
+        os.path.join(BASE_DIR, "automation/templates/ExampleTemplate1.html")
         print('\n\n\n\n\n\n\n\n mails sent\n\n\n')
     return render(request, 'home.html', {'data':data})
