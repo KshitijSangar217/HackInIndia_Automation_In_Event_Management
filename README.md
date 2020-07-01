936186cf139f32a233e0d11998a38328a2f394e8
Kshitij Sangar <kshitijsangar@gmail.com>
2020-07-01 15:35:33 +0530

    Added basic django setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# HackInIndia_Automation_In_Event_Management
-The project helps users to send Bulk html emails and Bulk ecertificates to their event participants. 
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\KSHITIJ R. SANGAR\Desktop\Kshitij Projects\Python projects\HackInIndia_Automation_In_Event_Management\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
