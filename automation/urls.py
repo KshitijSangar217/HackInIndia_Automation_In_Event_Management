3cd8c3e2f2d91815021e13a6876bd6d66ac717d2
Kshitij Sangar <kshitijsangar@gmail.com>
2020-07-01 17:29:38 +0530

    Email sent on click

--- automation/urls.py
+++ automation/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home),
+    path('/output',views.returndata, name='execute_email')
 ]
--- automation/views.py
+++ automation/views.py
@@ -4,18 +4,23 @@ from email.message import EmailMessage
 
 
 def home(request):
-    s = smtplib.SMTP("smtp.gmail.com", 587)
-    s.starttls()  # Traffic encryption
-    s.login("kshitijsangarofficial@gmail.com", "myfirstloverituu@mom.com")
-
-    msg = EmailMessage()
-    msg['Subject'] = "demo"
-    msg['From'] = "kshitijsangarofficial@gmail.com"
-    msg['To'] = "kshitijsangar@gmail.com"
-    gmail_content = "Hello guys"
-    msg.set_content(gmail_content)
-
-    s.send_message(msg)
-    s.quit()
-    print('\n\n\n\n\n\n\n\n mails sent\n\n\n')
     return render(request, 'home.html')
+
+def returndata(request):
+    if request.method == "POST":
+        s = smtplib.SMTP("smtp.gmail.com", 587)
+        s.starttls()  # Traffic encryption
+        s.login("kshitijsangarofficial@gmail.com", "YOURPASSWORD")
+
+        msg = EmailMessage()
+        msg['Subject'] = "demo"
+        msg['From'] = "kshitijsangarofficial@gmail.com"
+        msg['To'] = "kshitijsangar@gmail.com"
+        gmail_content = "Hello guys"
+        msg.set_content(gmail_content)
+
+        s.send_message(msg)
+        s.quit()
+        data = "sent to all"
+        print('\n\n\n\n\n\n\n\n mails sent\n\n\n')
+    return render(request, 'home.html', {'data':data})
